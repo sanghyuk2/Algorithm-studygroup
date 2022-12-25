44159f2d969e951019db2c3fb0d239ed48ea093d
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-25 23:43:09 +0900

    feat: 코드 6.5 소풍 문제를 해결하는 재귀 호출 코드

--- src/SangHyuk/종만북/무식하게풀기/소풍.java
+++ src/SangHyuk/종만북/무식하게풀기/소풍.java
@@ -1,31 +1,60 @@
 package SangHyuk.종만북.무식하게풀기;
 
 public class 소풍 {
+//    int n;
+//    boolean[][] areFriends = new boolean[10][10];
+//
+//    int countPairing(boolean taken[]) {
+//        boolean finished = true;
+//
+//        for (int i = 0; i < n; ++i) {
+//            if (!taken[i]) {
+//                finished = false;
+//            }
+//        }
+//
+//        if (finished) {
+//            return 1;
+//        }
+//
+//        int ret = 0;
+//
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = 0; j < n; ++j) {
+//                if (!taken[i] && !taken[j] && areFriends[i][j]) {
+//                    taken[i] = taken[j] = true;
+//                    ret += countPairing(taken);
+//                    taken[i] = taken[j] = false;
+//                }
+//            }
+//        }
+//        return ret;
+//    }
+
     int n;
     boolean[][] areFriends = new boolean[10][10];
 
-    int countPairing(boolean taken[]) {
-        boolean finished = true;
+    int countPairings(boolean[] taken) {
+        int firstFree = -1;
 
         for (int i = 0; i < n; ++i) {
             if (!taken[i]) {
-                finished = false;
+                firstFree = i;
+                break;
             }
         }
 
-        if (finished) {
+        if (firstFree == -1) {
             return 1;
         }
 
         int ret = 0;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (!taken[i] && !taken[j] && areFriends[i][j]) {
-                    taken[i] = taken[j] = true;
-                    ret += countPairing(taken);
-                    taken[i] = taken[j] = false;
-                }
+        for (int pairWith = firstFree + 1; pairWith < n; ++pairWith) {
+            if (!taken[pairWith] && areFriends[firstFree][pairWith]) {
+                taken[firstFree] = taken[pairWith] = true;
+                ret += countPairings(taken);
+                taken[firstFree] = taken[pairWith] = false;
             }
         }
         return ret;
