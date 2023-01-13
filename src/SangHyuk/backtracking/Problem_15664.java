64c0e253491ad15cc57bd95ab6813a986a3e8922
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-01-13 13:11:11 +0900

    feat: 백준 15651번 N과 M (10) 구현

--- src/SangHyuk/backtracking/Problem_15664.java
+++ src/SangHyuk/backtracking/Problem_15664.java
@@ -1,71 +1,62 @@
 package SangHyuk.backtracking;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Problem_15664 {
-    //구현 완료
-    //2일차에 다시 풀어보기 - 완료
-    //6일차에 다시 풀어보기
-    private static int n,m;
-    private static int[] map;
-    private static int[] result;
-    private static boolean[] visit;
+    static int N,M;
+    static int[] nums;
+    static HashSet<String> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        n = sc.nextInt();
-        m = sc.nextInt();
+        nums = new int[N];
 
-        map = new int[n];
-        result = new int[m];
-        visit = new boolean[n+1];
-
-        for (int i=0; i<n; i++) {
-            map[i] = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
         }
 
-        Arrays.sort(map);
+        Arrays.sort(nums);
 
-        StringBuilder sb = new StringBuilder();
+        pick(new boolean[N], new ArrayList<>(), 0);
 
-        cycle(0, 0, sb);
-
-        System.out.print(sb);
+        System.out.println(sb);
 
     }
 
-    private static void cycle(int start, int cnt, StringBuilder sb) {
-
-        if (cnt == m) {
-
-            for (int i=0; i<m; i++) {
-                sb.append(result[i] + " ");
-            }
-            sb.append("\n");
-
-        } else {
-
-            int num = 0;
-            for (int i=start; i<n; i++) {
-                if (!visit[i]) {
-                    if (num == map[i]) {
-                        continue;
-                    }
-
-                    visit[i] = true;
-                    result[cnt] = map[i];
-                    cycle(i+1, cnt+1, sb);
-                    visit[i] = false;
-
-                    num = map[i];
-
-                }
+    static void pick(boolean[] taken, ArrayList<Integer> picked, int toPick) {
+        if (toPick == M) {
+            //Collection을 여기다 사용함으로써 {7, 1}이던게 {1,7}이 되면서 picked.remove(picked.size()-1)에서 7이 삭제가 된다.
+            Collections.sort(picked);
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < toPick; i++) {
+                temp.append(picked.get(i)).append(' ');
             }
 
+            String str = temp.toString();
+
+            if (!set.contains(str)) {
+                set.add(str);
+                sb.append(temp).append('\n');
+            }
+
+            return;
         }
 
+        for (int next = 0; next < N; next++) {
+            if (taken[next]) {
+                continue;
+            }
+
+            picked.add(nums[next]);
+            taken[next] = true;
+            pick(taken, picked, toPick + 1);
+            //remove()에 int를 넘겨주면 index로 판단하므로 object형태인 Integer로 넘겨줘야한다.
+            picked.remove(Integer.valueOf(nums[next]));
+            taken[next] = false;
+        }
     }
 }
